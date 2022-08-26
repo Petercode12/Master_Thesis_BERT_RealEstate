0cc04be118994cb4113db137a77f8bfbd1deb160
Phi Tran Van (T) <PhiTran@dmchosting.ca>
2022-08-26 16:09:27 +0700

    init tools

---
+++ tools/ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json.Linq;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+
+
+var allLines = File.ReadAllLines("data.txt");
+
+StringBuilder stringBuilder = new StringBuilder();
+var listLines = new List<string>();
+
+var dupData = new List<string>();
+
+
+foreach (var str in allLines)
+{
+    JObject json = JObject.Parse(str);
+
+    var data = json["data"].ToString();
+
+    if (dupData.Contains(data))
+    {
+        continue;
+    }
+    dupData.Add(data);
+
+    var label = json["label"];
+
+
+    var listLabel = new List<string>();
+    foreach (var item in label)
+    {
+        listLabel.Add($"[\"{item[2]}\", \"{item[3]}\"]");
+    }
+    var labelString = String.Join(", \r\n", listLabel);
+
+    listLines.Add($@"{{
+    ""data"": ""{data.Replace("\t"," ")}"",
+    ""label"":[
+        {labelString}
+    ]
+}}");
+}
+var result = String.Join(", \r\n", listLines);
+using (StreamWriter writer = new StreamWriter("dataset.json"))
+{
+    writer.WriteLine("[");
+
+    writer.WriteLine(result);
+
+    writer.WriteLine("]");
+}
+
