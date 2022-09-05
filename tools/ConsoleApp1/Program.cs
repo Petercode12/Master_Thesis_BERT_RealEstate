e20d583c9f0c7dd56f4784ce6824f3e7f58e6f83
Phi Tran Van (T) <PhiTran@dmchosting.ca>
2022-09-05 11:49:11 +0700

    12345

--- tools/ConsoleApp1/Program.cs
+++ tools/ConsoleApp1/Program.cs
@@ -1,10 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Text;
 
 Console.WriteLine("Hello, World!");
 
-
+/*
 
 var allLines = File.ReadAllLines("data.txt");
 
@@ -52,4 +53,55 @@ using (StreamWriter writer = new StreamWriter("dataset.json"))
 
     writer.WriteLine("]");
 }
+*/
 
+
+var  allText = File.ReadAllText("dataset.json");
+
+var objects = JsonConvert.DeserializeObject<List<JObject>>(allText);
+
+var index = 0;
+
+var listJson = new List<string>();
+foreach (var json in objects)
+{
+    var data = json["data"].ToString();
+    var labels = json["label"];
+
+    var subStr = data;
+    var currentIndex = 0;
+    var listLabel = new List<string>();
+    foreach (var item in labels)
+    {
+        subStr = data.Substring(currentIndex, data.Length - currentIndex);
+        var labelName = item[0].ToString();
+        var labelValue = item[1].ToString();
+
+        try
+        {
+            var startIndex = subStr.IndexOf(labelValue) + currentIndex;
+            if (startIndex == 0 && currentIndex > 0)
+            {
+                throw new NullReferenceException(labelValue);
+            }
+
+            var endIndex = startIndex + labelValue.Length;
+            currentIndex = endIndex;
+            listLabel.Add($@"[{startIndex}, {endIndex}, ""{labelName}"", ""{labelValue}""]");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+            throw;
+        }
+    }
+    var labelString = $@"[{string.Join(',', listLabel)}]";
+    listJson.Add($@"{{""id"": {index}, ""data"": ""{data}"", ""label"": {labelString}}}");
+    index += 1;
+}
+
+var result = String.Join("\r\n", listJson);
+using (StreamWriter writer = new StreamWriter("labeled_dataset.jsonl"))
+{
+    writer.WriteLine(result);
+}
